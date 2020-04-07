d635765a01e7d449a0d788c3b054447a815262bb
Vitalii_Tymashkov <vitaliy.tima@gmail.com>
2020-04-07 20:38:30 +0200

    Another change in Helper.java

--- src/main/java/com/epam/util/Helper.java
+++ src/main/java/com/epam/util/Helper.java
@@ -1,4 +1,11 @@
 package com.epam.util;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Helper {
+
+//    Stached changesgit  2 - Only in NewBranch - Changes in master
+    public void start(){
+        System.out.println("Another change");
+    }
 }
